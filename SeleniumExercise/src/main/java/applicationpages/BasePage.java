5f5b0a51d0956723bd4889b0009a1592e4b4b28d
neha <ngoyal@venacorp.com>
2021-08-23 14:51:06 -0400

    code for element to be clickable

--- SeleniumExercise/src/main/java/applicationpages/BasePage.java
+++ SeleniumExercise/src/main/java/applicationpages/BasePage.java
@@ -1,7 +1,11 @@
 package applicationpages;
 
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
@@ -13,4 +17,22 @@ public class BasePage {
 		PageFactory.initElements(driver, this);
 		wait = new WebDriverWait(driver, 30, 500);
 	}
+
+	public void waitForElementClickable(WebElement element) {
+		ExpectedCondition<Boolean> condition = (unused -> {
+			try {
+				if (element.isDisplayed() && element.getAttribute("aria-disabled").equals(false)) {
+					return true;
+				} else {
+					return false;
+				}
+			} catch (ElementClickInterceptedException e) {
+				return false;
+			}
+		});
+		if (!wait.until(condition)) {
+			throw new TimeoutException("Element is not clickable.");
+		}
+	}
+
 }
